2b824c5d3e8efb3845f04d6066211e4b47a3e469
Saba24000 <2221648@iub.edu.bd>
2025-08-07 01:23:30 +0600

    New File added

---
+++ src/main/java/com/example/cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse/ProducersHomepagefxml.java
@@ -0,0 +1,7 @@
+package com.example.cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse;
+
+public class ProducersHomepagefxml
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
