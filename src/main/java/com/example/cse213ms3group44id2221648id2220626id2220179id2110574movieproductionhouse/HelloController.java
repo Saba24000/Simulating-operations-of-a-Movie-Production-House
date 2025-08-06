14c3c047d951ff8fd157c516eade89d914722aec
Saba24000 <2221648@iub.edu.bd>
2025-08-06 21:15:30 +0600

    init commit

---
+++ src/main/java/com/example/cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse to javafx.fxml;
+    exports com.example.cse213ms3group44id2221648id2220626id2220179id2110574movieproductionhouse;
+}
