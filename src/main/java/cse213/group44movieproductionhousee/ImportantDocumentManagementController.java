e4e7d3f028ad3624e98b2fc058417429a0b708b6
Saba24000 <2221648@iub.edu.bd>
2025-08-13 18:45:48 +0600

    git commit-m "Adding existing code"

--- src/main/java/cse213/group44movieproductionhousee/ImportantDocumentManagementController.java
+++ src/main/java/cse213/group44movieproductionhousee/ImportantDocumentManagementController.java
@@ -3,5 +3,31 @@ package cse213.group44movieproductionhousee;
 public class ImportantDocumentManagementController
 {
     @javafx.fxml.FXML
+    private Label projectNameLabel;
+    @javafx.fxml.FXML
+    private Label importantDocumentsLabel;
+    @javafx.fxml.FXML
+    private TableView projectDocsInfoTableView;
+    @javafx.fxml.FXML
+    private TableColumn documentsTableCol;
+    @javafx.fxml.FXML
+    private TextField projectNameTextField;
+    @javafx.fxml.FXML
+    private TableColumn projectNameTableCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void downloadPDFButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchButtonOnAction(ActionEvent actionEvent) {
+    }
+}
